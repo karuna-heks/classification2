2b9c42bc8a1c2de38bfa00e59a64ac8de2d6acc4
karuna-heks <56129973+karuna-heks@users.noreply.github.com>
2019-11-06 16:08:42 +0300

    Update NeuralNetwork.java

--- src/com/company/NeuralNetwork.java
+++ src/com/company/NeuralNetwork.java
@@ -15,7 +15,21 @@ public class NeuralNetwork {
         методы для коррекции весов синапсов
      */
 
+    /*
+        добавить методы learn или teach,
+        где на вход будет подаваться массив в виде словаря
+
+     */
+
+
+
     ReadSettings settings;
+
+    List<Neuron> iNeuronList;
+    List<Neuron> hNeuronList;
+    List<Neuron> oNeuronList;
+    List<ArrayList<Neuron>> hLayers;
+
 
     public NeuralNetwork(ReadSettings settings, int inputNumber, int outputNumber) {
 
@@ -24,16 +38,44 @@ public class NeuralNetwork {
             архитектуры нейронной сети
          */
 
-        this.settings = settings;
+        this.settings = settings; //получаем настройки НС
 
 
+        /*
+            добавить отдельную логику использования НС с 1, 2, или (3+) слоями
+         */
 
 
-        List<Neuron> iNeuronList = new ArrayList<>(inputNumber);
-        List<Neuron> hNeuronList = new ArrayList<>(settings.getNumberOfNeurons());
-        List<Neuron> oNeuronList = new ArrayList<>(outputNumber);
-        List<ArrayList<Neuron>> hLayers = new ArrayList<>(settings.getNumberOfHiddenLayers());//4648468468464684846468468
+        iNeuronList = new ArrayList<>(inputNumber);
+        hNeuronList = new ArrayList<>(settings.getNumberOfNeurons());
+        oNeuronList = new ArrayList<>(outputNumber);
+        hLayers = new ArrayList<>(settings.getNumberOfHiddenLayers());
+        //не желательно запаковывать нейроны внутри списка списков, т.к. будет тратиться
+        //много времени на лишнюю распаковку
+        //iNeuronList.set(1, Neuron);
 
+        Neuron inputNeuron = new Neuron(inputNumber);
+
+
+        /*
+            заполняем список нейронами и даём им случайные веса
+            необходимо это проделать для всех списков и всех нейронов
+            с учётом количества слоёв нейронной сети
+         */
+        for (int i = 0; i < iNeuronList.size(); i++) {
+            iNeuronList.set(i, inputNeuron);
+            iNeuronList.get(i).setRandomWeights();
+        }
+
+
+    }
+
+
+    public void calculation(double[] array) {
+
+        for (int i = 0; i < iNeuronList.size(); i++) {
+            iNeuronList.get(i).calcNeuron(array);
+        }
 
 
     }
